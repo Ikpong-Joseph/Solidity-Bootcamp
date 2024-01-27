e7bdb50c84fbc7bf2e4b4049cd63ce70a61f87df
Ikpong-Joseph <150890288+Ikpong-Joseph@users.noreply.github.com>
2024-01-27 23:36:39 +0100

    Updated RiseInProposalContract voting logic

--- RiseInProposalContract.sol
+++ RiseInProposalContract.sol
@@ -3,11 +3,14 @@ pragma solidity ^0.8.18;
 
 contract ProposalContract {
 
+    /*************************************DATA**************************************/
+
     uint256 private counter; // To keep track of proposal IDs, the uint256, in proposal_history.
     address public owner;  // Made public so to allow access in test.
     address[]  private voted_addresses; // Keep track of voted addresses.
-    
+    mapping(address => bool) public isVoted;
 
+ 
     struct Proposal {
         string title; // Task 1
         string description; // Description of the proposal
@@ -19,11 +22,14 @@ contract ProposalContract {
         bool is_active; // This shows if others can vote to our contract
     }
 
-    mapping(uint256 => Proposal) public proposal_history; // History of previous proposals
-    mapping(address => bool) public isVoted;
+    /*************************************MAPPINGS**************************************/
 
+    mapping(uint256 => Proposal) public proposal_history; // History of previous proposals
+    
+
+    /*************************************MODIFIERS**************************************/
    modifier onlyOwner() {
-        require(msg.sender == owner);
+        require(msg.sender == owner, "Only owner authorised to call this.");
         _;
     }
 
@@ -40,16 +46,32 @@ contract ProposalContract {
     constructor() {
         owner = msg.sender;
         voted_addresses.push(msg.sender); //Prevents owner from voting in their proposals.
+        isVoted[owner] = true; // This should be set to avoid owner from voting.
     }
+
+    /*************************************FUNCTIONS**************************************/
 
     // Task 2: Add title as function parameter.
     function create(string memory _title, string calldata _description, uint256 _total_vote_to_end) external onlyOwner {
-        counter += 1; // No proposal will be found at proposal_history[0]. Unless hardcoded in constructor and counter logic moved 2lines down.
+        counter += 1; // No proposal will be found at proposal_history[0]. For getProposal and proposal_history, first proposal starts at 1. Not 0.
         proposal_history[counter] = Proposal(_title, _description, 0, 0, 0, _total_vote_to_end, false, true);
     }
 
     function setNewOwner(address new_owner) external onlyOwner {
         owner = new_owner;
+        uint i; 
+        for (i = 0; i < voted_addresses.length; i++) {
+            if (msg.sender == voted_addresses[i]) {
+                delete voted_addresses[i]; // Remove msg.sender from [];
+            }
+            isVoted[msg.sender] = false; // Remove msg.sender (Former owner) from mapping.
+            if (owner == voted_addresses[i]) {
+                revert("Voters cannot own this proposal.");
+            }
+        }
+        voted_addresses.push(owner);  // Add new owner to the list
+        isVoted[owner] = true; // And mapping.
+        
     }
 
     function vote(uint8 choice) external isActive newVoter(msg.sender){
@@ -58,7 +80,9 @@ contract ProposalContract {
         Proposal storage proposal = proposal_history[counter];
         uint256 total_vote = proposal.approve + proposal.reject + proposal.pass;
 
+        // Add caller to both [] and mapping
         voted_addresses.push(msg.sender);
+        isVoted[msg.sender] = true;
 
         /*
         Second part
@@ -127,9 +151,6 @@ contract ProposalContract {
             }
         }
         return false;
-
-        
-        
     }
 
     // Basic getter function to retrieve the current proposal
@@ -143,3 +164,5 @@ contract ProposalContract {
     }
     
 }
+
+// Deployed on BNB testnet at 0x5E71e1182980275ea48A6eF5B51D03C8F6335864
